697ddf1f62c2fcf241d9773c6433aa7307b56488
Ikraised <Ikraised@yandex.ru>
2020-08-12 18:02:50 +0300

    1111

---
+++ public/js/script.js
@@ -0,0 +1,6 @@
+"use strict";
+
+$(function () {
+    // $('.route-slider').slick();
+});
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNjcmlwdC5qcyJdLCJuYW1lcyI6WyIkIl0sIm1hcHBpbmdzIjoiOztBQUFBQSxFQUFFLFlBQVk7QUFDVjtBQUNILENBRkQiLCJmaWxlIjoic2NyaXB0LmpzIiwic291cmNlc0NvbnRlbnQiOlsiJChmdW5jdGlvbiAoKSB7XG4gICAgLy8gJCgnLnJvdXRlLXNsaWRlcicpLnNsaWNrKCk7XG59KSJdfQ==
